8f71cf11bda7c30ad94a95b2a7e295aa2f7260bd
Lukasz <lukaszmaczka3604@gmail.com>
2021-04-11 01:59:09 +0200

    Continuation of work on password reset page
    Added some gui elements and created methods to handle actions

--- gui/Projekt/src/main/java/controllers/auth/PasswordReset.java
+++ gui/Projekt/src/main/java/controllers/auth/PasswordReset.java
@@ -3,6 +3,9 @@ package main.java.controllers.auth;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import main.java.SceneManager;
 
@@ -11,6 +14,27 @@ public class PasswordReset {
     @FXML
     private Button passwordResetReturnButtonButton;
 
+    public void handleSendVerificationCode(ActionEvent actionEvent) {
+    }
+
+    public void handleVerify(ActionEvent actionEvent) {
+    }
+
+    public void passwordReset(){
+        System.out.println("Zresetowano");
+    }
+
+    public void handleSetNewPassword(ActionEvent actionEvent) {
+        passwordReset();
+    }
+
+    public void handlePasswordResetOnEnterPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.ENTER)
+        {
+            passwordReset();
+        }
+    }
+
     public void handleReturn(ActionEvent actionEvent) {
         SceneManager.renderScene("login");
     }
@@ -18,5 +42,17 @@ public class PasswordReset {
     public void handleExit(ActionEvent actionEvent) {
         Stage stage = (Stage) passwordResetReturnButtonButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void clearErrorsOnEmail(KeyEvent keyEvent) {
+    }
+
+    public void clearErrorsOnVerificationCode(KeyEvent keyEvent) {
+    }
+
+    public void clearErrorsOnPassword(KeyEvent keyEvent) {
+    }
+
+    public void clearErrorsOnRepeatPassword(KeyEvent keyEvent) {
     }
 }
