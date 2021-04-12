f0b12711a7fd5f0e003d4d68b5870ed3d8464b0f
Jan Nowak$ git config --global user.name Jan <filippapiernik1999@gmail.com>
2021-04-12 18:33:50 +0200

    Statuses in specific package are now created dynamically (still missing db connection)

--- gui/Projekt/src/main/java/controllers/client/ClientTrackPackage.java
+++ gui/Projekt/src/main/java/controllers/client/ClientTrackPackage.java
@@ -11,8 +11,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import main.java.features.Animations;
 
 import java.io.IOException;
@@ -39,7 +41,7 @@ public class ClientTrackPackage implements Initializable {
     private AnchorPane moreInformationPane;
 
     @FXML
-    private Label test;
+    private VBox statusesVBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -77,10 +79,24 @@ public class ClientTrackPackage implements Initializable {
                 showMore.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
+
                         Animations.changePane(trackPackagePane,moreInformationPane,-850,0.5);
-                        test.setText(packageItem.getNumber());
+
                         btnBack.setVisible(true);
                         btnBack.setOpacity(1);
+
+                        //Testing dynamically created statuses from list that's gonna be filled with db rows
+                        createStatus("28 Mar 2021 23:00", "Przesyłka zarejestrowana");
+                        createStep(2);
+                        createStatus("28 Mar 2021 23:00", "Odebrana od klienta");
+                        createStep(2);
+                        createStatus("28 Mar 2021 23:00", "Przyjęta w oddziale");
+                        createStep(2);
+                        createStatus("28 Mar 2021 23:00", "W transporcie");
+                        createStep(2);
+                        createStatus("28 Mar 2021 23:00", "W doręczeniu");
+                        createStep(4);
+                        createCurrentStatus("28 Mar 2021 23:00", "Dostarczona - " + packageItem.getNumber(),"Podróż przesyłki od Nadawcy do Obiorcy zakończyła się. Dziękujemy!");
                     }
                 });
                 pane.getChildren().add(1,showMore);
@@ -92,6 +108,124 @@ public class ClientTrackPackage implements Initializable {
             }
         }
 
+    }
+
+    private void createStatus(String date, String status){
+
+        HBox statusHBox = new HBox();
+
+        Pane dataPane = new Pane();
+        Pane squarePane = new Pane();
+        Pane grayPane = new Pane();
+        Pane statusPane = new Pane();
+
+        Label dataLabel = new Label();
+        Label statusName = new Label();
+
+        dataLabel.setText(date);
+        dataLabel.getStyleClass().add("textOption");
+        dataLabel.setLayoutX(8);
+        dataLabel.setLayoutY(3);
+
+        statusName.setText(status + ".");
+        statusName.getStyleClass().add("optionGroup");
+
+        grayPane.setPrefWidth(30);
+        grayPane.setPrefHeight(27);
+        grayPane.setLayoutX(32);
+        grayPane.getStyleClass().add("grayColor");
+
+        squarePane.setPrefWidth(85);
+        squarePane.getChildren().add(grayPane);
+
+        statusPane.getChildren().add(statusName);
+
+        dataPane.setPrefWidth(147);
+        dataPane.getChildren().add(dataLabel);
+
+        statusHBox.setPrefHeight(36);
+        statusHBox.getChildren().add(dataPane);
+        statusHBox.getChildren().add(squarePane);
+        statusHBox.getChildren().add(statusPane);
+
+        statusesVBox.getChildren().add(0,statusHBox);
+    }
+
+    private void createStep(int steps){
+        for(int i = 0 ; i < steps; i ++) {
+            HBox stepBox = new HBox();
+
+            Pane emptyPane = new Pane();
+            Pane squarePane = new Pane();
+            Pane grayPane = new Pane();
+
+            emptyPane.setPrefWidth(165);
+
+            grayPane.setPrefWidth(9);
+            grayPane.setPrefHeight(9);
+            grayPane.setLayoutX(24);
+            grayPane.setLayoutY(1);
+            grayPane.getStyleClass().add("grayColor");
+
+            squarePane.getChildren().add(grayPane);
+
+            stepBox.setPrefHeight(18);
+            stepBox.getChildren().add(emptyPane);
+            stepBox.getChildren().add(squarePane);
+
+            statusesVBox.getChildren().add(0,stepBox);
+        }
+    }
+
+    private void createCurrentStatus(String date, String status, String desc){
+
+        HBox statusHBox = new HBox();
+
+        Pane dataPane = new Pane();
+        Pane squarePane = new Pane();
+        Pane currentPane = new Pane();
+        Pane statusPane = new Pane();
+
+        Label dataLabel = new Label();
+        Label statusName = new Label();
+
+        Text descriptionOfStatus = new Text(desc);
+
+        descriptionOfStatus.setWrappingWidth(450);
+        descriptionOfStatus.getStyleClass().add("textOption");
+        descriptionOfStatus.setLayoutX(6);
+        descriptionOfStatus.setLayoutY(46);
+
+        dataLabel.setText(date);
+        dataLabel.getStyleClass().add("textOption");
+        dataLabel.setLayoutX(8);
+        dataLabel.setLayoutY(27);
+
+        statusName.setText(status + ".");
+        statusName.getStyleClass().add("optionGroup");
+        statusName.setLayoutX(5);
+
+        currentPane.setPrefWidth(57);
+        currentPane.setPrefHeight(51);
+        currentPane.setLayoutX(18);
+        currentPane.setLayoutY(11);
+        currentPane.getStyleClass().add("sideBackground");
+
+        squarePane.setPrefWidth(85);
+        squarePane.getChildren().add(currentPane);
+
+        statusPane.getChildren().add(statusName);
+        statusPane.getChildren().add(descriptionOfStatus);
+
+        dataPane.setPrefWidth(147);
+        dataPane.getChildren().add(dataLabel);
+
+        statusHBox.setPrefHeight(73);
+        statusHBox.getChildren().add(dataPane);
+        statusHBox.getChildren().add(squarePane);
+        statusHBox.getChildren().add(statusPane);
+
+        statusesVBox.getChildren().add(0,statusHBox);
     }
 
     //Filing list with example data
@@ -123,5 +257,8 @@ public class ClientTrackPackage implements Initializable {
     void backToTrackPackage(ActionEvent event) throws IOException {
         Animations.fadeAway(btnBack,0.5,1,0,false);
         Animations.changePane(moreInformationPane,trackPackagePane,+850,0.5);
+
+        //Need rework
+        statusesVBox.getChildren().clear();
     }
 }
