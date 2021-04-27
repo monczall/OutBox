0e409dc0a39e10e841203138fa56c12660b3fd86
Blade669 <damianpaluch98@gmail.com>
2021-04-27 10:12:11 +0200

    searching in 'change status'

--- gui/Projekt/src/main/java/controllers/courier/CourierSecond.java
+++ gui/Projekt/src/main/java/controllers/courier/CourierSecond.java
@@ -1,17 +1,15 @@
 package main.java.controllers.courier;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 import main.java.dao.PackagesDAO;
 import main.java.entity.Packages;
 import org.controlsfx.control.table.TableRowExpanderColumn;
@@ -22,42 +20,37 @@ import java.util.ResourceBundle;
 
 public class CourierSecond implements Initializable {
 
+    private static int id;
+    private static String comment;
+    Pane pane;
+    private final ObservableList<Packages> packages = PackagesDAO.addTable();
     @FXML
     private TableView<Packages> table;
     @FXML
     private TableColumn<?, ?> packageNumber;
-
     @FXML
     private TableColumn<?, ?> name;
-
     @FXML
     private TableColumn<?, ?> surname;
-
     @FXML
     private TableColumn<?, ?> city;
-
     @FXML
     private TableColumn<?, ?> address;
-
     @FXML
     private TableColumn<?, ?> telephone;
-
     @FXML
     private TableColumn<?, ?> state;
-
     @FXML
     private TableColumn<?, ?> time;
-
-    Pane pane;
-    private static int id;
-    private static String comment;
+    @FXML
+    private TextField searchField;
 
     public static int getId() {
         return id;
     }
 
     public void setId(int id) {
-        this.id = id;
+        CourierSecond.id = id;
     }
 
     public static String getComment() {
@@ -69,7 +62,7 @@ public class CourierSecond implements Initializable {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
         TableRowExpanderColumn<Packages> expanderRow = new TableRowExpanderColumn<Packages>(this::createEditor);
 
         packageNumber.setCellValueFactory(new PropertyValueFactory<>("packageNumber"));
@@ -82,15 +75,33 @@ public class CourierSecond implements Initializable {
         telephone.setCellValueFactory(new PropertyValueFactory<>("phone"));
         table.getColumns().add(expanderRow);
         table.setItems(PackagesDAO.addTable());
+        table.getSelectionModel().select(0);
     }
 
-    private Pane createEditor(TableRowExpanderColumn.TableRowDataFeatures<Packages> arg){
-        try{
+    @FXML
+    void search(KeyEvent event) {
+        table.getItems().clear();
+        for (int i = 0; i < packages.size(); i++) {
+            if (packages.get(i).getPackageNumber().contains(searchField.getText()) ||
+                    packages.get(i).getName().contains(searchField.getText()) ||
+                    packages.get(i).getSurname().contains(searchField.getText()) ||
+                    packages.get(i).getCity().contains(searchField.getText()) ||
+                    packages.get(i).getAddress().contains(searchField.getText()) ||
+                    packages.get(i).getPhone().contains(searchField.getText()) ||
+                    packages.get(i).getStatus().contains(searchField.getText()) ||
+                    packages.get(i).getTimeOfPlannedDelivery().contains(searchField.getText())) {
+                table.getItems().add(packages.get(i));
+            }
+        }
+    }
+
+    private Pane createEditor(TableRowExpanderColumn.TableRowDataFeatures<Packages> arg) {
+        try {
             table.getSelectionModel().select(arg.getTableRow().getIndex());
             setId(table.getItems().get(arg.getTableRow().getIndex()).getUserId());
             setComment(table.getItems().get(arg.getTableRow().getIndex()).getAdditionalComment());
             pane = FXMLLoader.load(getClass().getResource("../../../resources/view/courier/expandableRow.fxml"));
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return pane;
