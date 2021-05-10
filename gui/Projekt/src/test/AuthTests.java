af292ff37c42e3db4691ecb1bb472162de6b9e28
Lukasz <lukaszmaczka3604@gmail.com>
2021-05-10 11:56:52 +0200

    Added new tests and separated database tests from others

--- gui/Projekt/src/test/AuthTests.java
+++ gui/Projekt/src/test/AuthTests.java
@@ -1,236 +1,110 @@
 package test;
 
-import main.java.dao.HibernateUtil;
-import main.java.entity.UserInfos;
-import main.java.entity.Users;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import main.java.controllers.auth.Login;
 import org.junit.jupiter.api.*;
+import org.opentest4j.AssertionFailedError;
 
-import java.util.List;
+public class AuthTests extends Login {
 
-public class AuthTests {
+    @Test
+    public void shouldBeEmail(){
+        System.out.println("Test sprawdzający czy string jest emailem");
 
-    private static SessionFactory sessionFactory;
-    private Session session;
+        String email = "email@gmail.com";
 
-    @BeforeAll
-    public static void setUp() {
-        sessionFactory = HibernateUtil.getSessionFactory();
-        System.out.println("Utworzono sessionFactory");
-    }
-
-    @BeforeEach
-    public void openSession() {
-        session = sessionFactory.openSession();
-        System.out.println("");
-        System.out.println("=============");
-        System.out.println("Sesja otwarta");
-    }
-
-    @AfterEach
-    public void closeSession() {
-        if(session != null)
-        {
-            session.close();
-            System.out.println("Sesja zamknieta");
-            System.out.println("===============");
-        }
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        if(sessionFactory != null){
-            sessionFactory.close();
-            System.out.println("Zamknięto sessionFactory");
-        }
+        Assertions.assertTrue(isEmail(email));
     }
 
     @Test
-    public void ReadUsersTest() {
-        System.out.println("Test pobierający użytkowników...");
+    public void shouldNotBeEmail(){
+        System.out.println("Test sprawdzający czy string jest emailem");
 
-        session.beginTransaction();
+        String email = "email!gmail.com";
 
-        Query query = session.createQuery("from Users");
-
-        List<Users> listOfAllUsers = query.list();
-
-        Assertions.assertNotNull(listOfAllUsers);
+        Assertions.assertFalse(isEmail(email));
     }
 
     @Test
-    public void ReadUserInfosTest() {
-        System.out.println("Test pobierający informacje na temat użytkowników...");
+    public void loginAndPasswordShouldNotBeEmptyTest(){
+        System.out.println("Test sprawdzający czy podano login i haslo...");
 
-        session.beginTransaction();
+        String email = "email@gmail.com";
+        String password = "Strong_Password2132!";
 
-        Query query = session.createQuery("from UserInfos ");
-
-        List<UserInfos> listOfUserInfos = query.list();
-
-        Assertions.assertNotNull(listOfUserInfos);
+        Assertions.assertFalse(isEmpty(email, password));
     }
 
     @Test
-    public void ReadNonExistingUserTest() {
-        System.out.println("Test pobierający nieistniejącego użytkownika...");
+    public void shouldThrowErrorWhenLoginIsEmptyTest(){
+        System.out.println("Test sprawdzający czy wystapi blad jezeli nie podano adresu email...");
 
-        session.beginTransaction();
+        String email = "";
+        String password = "Strong_Password2132!";
 
-        String email = "takiuzytkowniknieistnieje@gmail.com";
-
-        Query query = session.createQuery("from Users u where u.email = :email");
-        query.setParameter("email",email);
-
-        List<Users> listOfNonExistingUser = query.list();
-
-        Assertions.assertEquals(0,listOfNonExistingUser.size());
+        Assertions.assertThrows(NullPointerException.class,
+                ()->{
+                    isEmpty(email, password);
+                }
+        );
     }
 
     @Test
-    public void ReadClientUserTest() {
-        System.out.println("Test pobierający istniejącego użytkownika - klient...");
+    public void shouldThrowErrorWhenPasswordIsEmptyTest(){
+        System.out.println("Test sprawdzający czy wystapi blad jezeli nie podano hasla...");
 
-        session.beginTransaction();
+        String email = "email@gmail.com";
+        String password = "";
 
-        String email = "klient@gmail.com";
-
-        Query query = session.createQuery("from Users u where u.email = :email");
-        query.setParameter("email",email);
-
-        List<Users> listOfClientUser = query.list();
-
-        Assertions.assertEquals(1,listOfClientUser.size());
+        Assertions.assertThrows(NullPointerException.class,
+                ()->{
+                    isEmpty(email, password);
+                }
+        );
     }
 
     @Test
-    public void ReadCourierUserTest() {
-        System.out.println("Test pobierający istniejącego użytkownika - kurier...");
+    public void shouldReturnUserId(){
+        System.out.println("Test sprawdzający czy zwrocono id uzytkownika o numerze '1'...");
+        setUserID(1);
 
-        session.beginTransaction();
-
-        String email = "kurier@gmail.com";
-
-        Query query = session.createQuery("from Users u where u.email = :email");
-        query.setParameter("email",email);
-
-        List<Users> listOfCourierUser = query.list();
-
-        Assertions.assertEquals(1,listOfCourierUser.size());
+        Assertions.assertEquals(1,getUserID());
     }
 
     @Test
-    public void ReadManagerUserTest() {
-        System.out.println("Test pobierający istniejącego użytkownika - menadzer...");
+    public void shouldBeErrorWhileReturningWrongUserId(){
+        System.out.println("Test sprawdzający czy wystąpił błąd przy zwroceniu złego id uzytkownika...");
+        setUserID(1);
 
-        session.beginTransaction();
-
-        String email = "menadzer@gmail.com";
-
-        Query query = session.createQuery("from Users u where u.email = :email");
-        query.setParameter("email",email);
-
-        List<Users> listOfManagerUser = query.list();
-
-        Assertions.assertEquals(1,listOfManagerUser.size());
+        Assertions.assertThrows(AssertionFailedError.class,
+                ()->{
+                    Assertions.assertEquals(2,getUserID());
+                });
     }
 
     @Test
-    public void ReadAdminUserTest() {
-        System.out.println("Test pobierający istniejącego użytkownika - administrator...");
+    public void shouldReturnUserInfoId(){
+        System.out.println("Test sprawdzający czy zwrocono user info id o numerze '1'...");
+        setUserInfoID(1);
 
-        session.beginTransaction();
-
-        String email = "administrator@gmail.com";
-
-        Query query = session.createQuery("from Users u where u.email = :email");
-        query.setParameter("email",email);
-
-        List<Users> listOfAdministratorUser = query.list();
-
-        Assertions.assertEquals(1,listOfAdministratorUser.size());
+        Assertions.assertEquals(1,getUserInfoID());
     }
 
     @Test
-    public void ReadNonExistingUserInfoTest() {
-        System.out.println("Test pobierający informacje na temat nieistniejacego użytkownika");
+    public void shouldBeErrorWhileReturningWrongUserInfoId(){
+        System.out.println("Test sprawdzający czy wystąpił błąd przy zwroceniu złego user info id...");
+        setUserInfoID(1);
 
-        session.beginTransaction();
-
-        int id = 9999;
-
-        Query query = session.createQuery("from UserInfos ui where ui.id = :id");
-        query.setParameter("id",id);
-
-        List<UserInfos> listOfNonExistingUserInfo = query.list();
-
-        Assertions.assertEquals(0,listOfNonExistingUserInfo.size());
+        Assertions.assertThrows(AssertionFailedError.class,
+                ()->{
+                    Assertions.assertEquals(2,getUserInfoID());
+                });
     }
 
     @Test
-    public void ReadClientUserInfoTest() {
-        System.out.println("Test pobierający informacje na temat użytkownika - klient");
+    public void shouldReturnUserEmail(){
+        System.out.println("Test sprawdzający czy zwrocono email uzytkownika...");
+        setUserEmail("email@gmail.com");
 
-        session.beginTransaction();
-
-        int id = 1;
-
-        Query query = session.createQuery("from UserInfos ui where ui.id = :id");
-        query.setParameter("id",id);
-
-        List<UserInfos> listOfClientUserInfo = query.list();
-
-        Assertions.assertEquals(1,listOfClientUserInfo.size());
+        Assertions.assertNotNull(getUserEmail());
     }
-
-    @Test
-    public void ReadCourierUserInfoTest() {
-        System.out.println("Test pobierający informacje na temat użytkownika - kurier");
-
-        session.beginTransaction();
-
-        int id = 2;
-
-        Query query = session.createQuery("from UserInfos ui where ui.id = :id");
-        query.setParameter("id",id);
-
-        List<UserInfos> listOfCourierUserInfo = query.list();
-
-        Assertions.assertEquals(1,listOfCourierUserInfo.size());
-    }
-
-    @Test
-    public void ReadManagerUserInfoTest() {
-        System.out.println("Test pobierający informacje na temat użytkownika - menadzer");
-
-        session.beginTransaction();
-
-        int id = 3;
-
-        Query query = session.createQuery("from UserInfos ui where ui.id = :id");
-        query.setParameter("id",id);
-
-        List<UserInfos> listOfManagerUserInfo = query.list();
-
-        Assertions.assertEquals(1,listOfManagerUserInfo.size());
-    }
-
-    @Test
-    public void ReadAdministratorUserInfoTest() {
-        System.out.println("Test pobierający informacje na temat użytkownika - administrator");
-
-        session.beginTransaction();
-
-        int id = 4;
-
-        Query query = session.createQuery("from UserInfos ui where ui.id = :id");
-        query.setParameter("id",id);
-
-        List<UserInfos> listOfAdministratorUserInfo = query.list();
-
-        Assertions.assertEquals(1,listOfAdministratorUserInfo.size());
-    }
-
 }
