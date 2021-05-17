3e698fe27a10dcbe204817b3ec75a4fa39c3c05f
Lukasz <lukaszmaczka3604@gmail.com>
2021-05-17 20:58:35 +0200

    Database update

--- gui/Projekt/database/populate_user_infos.sql
+++ gui/Projekt/database/populate_user_infos.sql
@@ -2,6 +2,10 @@ USE outbox;
 INSERT INTO `user_infos` (`ID`, `name`, `surname`, `phone_number`, `street_and_number`, `city`, `voivodeship`) VALUES
 (1, 'Jan', 'Kowalski', '789456321', 'Mickiewicza 90', 'Rzeszow', 'Podkarpackie'),
 (2, 'Adam', 'Adamiak', '432198765', 'Lwowska 123', 'Rzeszow', 'Podkarpackie'),
-(3, 'Jakub', 'Szmul', '456789321', 'Jana Pawla 21', 'Krakow', 'Malopolskie'),
-(4, 'Karol', 'Wilk', '231564879', 'Warola Kilka 32', 'Lezajsk', 'Podkarpackie'),
-(5, 'Piotr', 'Lewandowski', '678091546', 'Sassanki 57', 'Rzeszow', 'Podkarpackie');
+(3, 'Kamil', 'Bereziewicz', '432132865', 'Armatnia 12', 'Rzeszow', 'Podkarpackie'),
+(4, 'Jakub', 'Szmul', '456789321', 'Jana Pawla 21', 'Krakow', 'Malopolskie'),
+(5, 'Karol', 'Wilk', '231564879', 'Warola Kilka 32', 'Rzeszow', 'Podkarpackie'),
+(6, 'Piotr', 'Lewandowski', '678091546', 'Sassanki 57', 'Krakow', 'Malopolskie'),
+(7, 'Stefan', 'Szmul', '678091543', 'Fajna 57', 'Rzeszow', 'Podkarpackie'),
+(8, 'Andrzej', 'Kowalski', '678091544', 'Rejtana 57', 'Krakow', 'Malopolskie'),
+(9, 'Michal', 'Kozak', '678091123', 'Fajnancka 547', 'Warszawa', 'Mazowieckie');
--- gui/Projekt/database/populate_users.sql
+++ gui/Projekt/database/populate_users.sql
@@ -1,6 +1,14 @@
 USE outbox;
 INSERT INTO `users` (`ID`, `user_infoID`, `areaID`, `email`, `password`, `role`) VALUES
 (1, 1, NULL, 'klient@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Klient'),
-(2, 2, 1, 'kurier@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
-(3, 3, 2, 'menadzer@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
-(4, 4, 3, 'administrator@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Administrator');
+(2, 2, 1, 'k.rzeszow1@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(3, 3, 1, 'k.rzeszow2@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(4, 4, 4, 'k.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(5, 5, 3, 'ko.podkarpacie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier Międzyoddziałowy'),
+(6, 6, 5, 'ko.maloposkie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier Międzyoddziałowy'),
+(7, 7, 1, 'm.rzeszow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(8, 8, 4, 'm.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(9, 9, NULL, 'admin@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Administrator');
+
+
+
