6bed2484f4231f7eef48ff29cba96c5cdb03188a
Lukasz <lukaszmaczka3604@gmail.com>
2021-05-18 13:00:25 +0200

    Database update

--- gui/Projekt/database/populate_users.sql
+++ gui/Projekt/database/populate_users.sql
@@ -1,14 +1,20 @@
 USE outbox;
 INSERT INTO `users` (`ID`, `user_infoID`, `areaID`, `email`, `password`, `role`) VALUES
-(1, 1, NULL, 'klient@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Klient'),
-(2, 2, 1, 'k.rzeszow1@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
-(3, 3, 1, 'k.rzeszow2@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
-(4, 4, 4, 'k.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
-(5, 5, 3, 'ko.podkarpacie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier Międzyoddziałowy'),
-(6, 6, 5, 'ko.maloposkie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier Międzyoddziałowy'),
-(7, 7, 1, 'm.rzeszow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
-(8, 8, 4, 'm.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
-(9, 9, NULL, 'admin@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Administrator');
+(1, 1, NULL, 'klient.rzeszow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Klient'),
+(2, 2, NULL, 'klient.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Klient'),
+(3, 3, NULL, 'klient.kolbuszowa@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Klient'),
+(4, 4, 2, 'k.rzeszow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(5, 5, 2, 'k.rzeszow2@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(6, 6, 3, 'k.kolbuszowa@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(7, 7, 5, 'k.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier'),
+(8, 8, 4, 'ko.podkarpacie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier Międzyoddziałowy'),
+(9, 9, 6, 'ko.maloposkie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Kurier Międzyoddziałowy'),
+(10, 10, 2, 'm.rzeszow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(11, 11, 3, 'm.kolbuszowa@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(12, 12, 4, 'm.podkarpacie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(13, 13, 5, 'm.krakow@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(14, 14, 6, 'm.malopolskie@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Menadżer'),
+(15, 15, NULL, 'admin@gmail.com', '9e38e8d688743e0d07d669a1fcbcd35b', 'Administrator');
 
 
 
